860d72453193d404b2e63284976f998b360c839e
Aakash Garg <iaakashg55@gmail.com>
2020-08-20 20:58:20 +0530

    Completed module

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const viewPath = path.join(__dirname + '/views');
 app.set('views', viewPath);
 app.set('view engine','ejs');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => { 
     res.render('index', {
